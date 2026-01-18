be795db9f7875dccfe19bb34cce4dccb01bb05c0
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-18 08:37:15 +0600

    C-sharp if_else and loop

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //    string message = "Hello world";
+        //    Console.WriteLine(message);
+        //    Console.WriteLine("Hello World!");
+        //    if (3 > 1)
+        //    {
+        //        Console.WriteLine("Yes");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("NO");        }
+        //
+        //string name;
+
+        //Console.WriteLine("Enter your name");
+        //name = Console.ReadLine();
+        //Console.WriteLine("Enter ypur Income");
+
+
+        //int nummber = Convert.ToInt32((string)Console.ReadLine());
+
+
+
+        //Console.WriteLine(nummber + 100);
+
+        //Console.WriteLine(name);
+
+        for (int i = 0; i < 9; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine(1650+200+300+200+300);
+    }
+}
