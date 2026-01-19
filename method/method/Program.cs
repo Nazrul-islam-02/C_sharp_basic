6041b9be564830e07a408a61cbac9355165a35c1
Nazrul-islam-02 <nnazrulict19@gmail.com>
2026-01-19 12:15:16 +0600

    method

---
+++ method/method/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(Sum(12, 23));
+            //Add(12,34)
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
